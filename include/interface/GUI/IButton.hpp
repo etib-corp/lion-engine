404695acfd7326764c3a2029d8fb27950cae8840
Neisan <nathan.maillot@epitech.eu>
2024-10-28 22:56:00 +0400

    feat: add IButton and SFMLButton classes

--- include/interface/GUI/IButton.hpp
+++ include/interface/GUI/IButton.hpp
@@ -19,6 +19,8 @@ namespace LE {
             public:
                 virtual ~IButton() = default;
 
+                virtual void init() = 0;
+
                 void onClick() override;
 
                 void onHover() override;
