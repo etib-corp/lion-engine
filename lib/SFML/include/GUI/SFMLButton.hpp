0f2e80ea641b89c926a6df9d24bd8ed3d84b25ee
Neisan <nathan.maillot@epitech.eu>
2024-10-29 23:23:49 +0400

    chore: add missing documentation on SFML module

--- lib/SFML/include/GUI/SFMLButton.hpp
+++ lib/SFML/include/GUI/SFMLButton.hpp
@@ -15,11 +15,36 @@
 
 namespace LE {
     namespace GUI {
+        /**
+         * @brief The SFMLButton class
+         *
+         * The SFMLButton class is a class that inherits from the IButton interface.
+         *
+         * It's used to create a button on the screen.
+         *
+        */
         class SFMLButton : public IButton {
             public:
+                /**
+                 * @brief Construct a new SFMLButton object
+                 *
+                 * @param pos The position of the button
+                 * @param size The size of the button
+                 * @param content The content of the button
+                 */
                 SFMLButton(const LE::Vector3<float> &pos, const LE::Vector2<float> &size, const std::string &content);
+
+                /**
+                 * @brief Destroy the SFMLButton object
+                 *
+                 */
                 ~SFMLButton();
 
+                /**
+                 * @brief Initialize the button.
+                 *
+                 * @return void
+                 */
                 void init() override;
         };
     }
--- lib/SFML/include/GUI/SFMLTextField.hpp
+++ lib/SFML/include/GUI/SFMLTextField.hpp
@@ -13,9 +13,29 @@
 
 namespace LE {
     namespace GUI {
+        /**
+         * @brief The SFMLTextField class
+         *
+         * The SFMLTextField class is a class that inherits from the ITextField interface.
+         *
+         * It's used to create a text field on the screen.
+         *
+        */
         class SFMLTextField : public ITextField {
             public:
+                /**
+                 * @brief Construct a new SFMLTextField object
+                 *
+                 * @param pos The position of the text field
+                 * @param label The label of the text field
+                 * @param placeholder The placeholder of the text field
+                 */
                 SFMLTextField(const LE::Vector3<float> &pos, const std::string &label = "Label", const std::string &placeholder = "Enter text");
+
+                /**
+                 * @brief Destroy the SFMLTextField object
+                 *
+                 */
                 ~SFMLTextField();
 
         };
