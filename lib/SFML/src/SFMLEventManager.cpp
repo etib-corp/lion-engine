2d83293c21ecc62d5f388679b56c6ebd68e0bc1b
ju-lit-hein <julien.ferdinand@epitech.eu>
2024-10-31 18:12:32 +0400

    fix: eventManager

--- lib/SFML/src/SFMLEventManager.cpp
+++ lib/SFML/src/SFMLEventManager.cpp
@@ -29,20 +29,22 @@ void LE::SFMLEventManager::pollEvents()
             if (_event.type == sf::Event::KeyPressed) {
                 if (_event.key.code == key->key && key->type == LE::JUST_PRESSED && !key->_alreadyPressed) {
                     callback(_engine, 0);
+                    key->_alreadyPressed = true;
                 }
                 if (_event.key.code == key->key && key->type == LE::PRESSED) {
                     callback(_engine, 0);
+                    key->_alreadyPressed = true;
                 }
-                key->_alreadyPressed = true;
             }
             if (_event.type == sf::Event::KeyReleased) {
                 if (_event.key.code == key->key && key->type == LE::JUST_RELEASED && key->_alreadyPressed) {
                     callback(_engine, 0);
+                    key->_alreadyPressed = false;
                 }
                 if (_event.key.code == key->key && key->type == LE::RELEASED) {
                     callback(_engine, 0);
+                    key->_alreadyPressed = false;
                 }
-                key->_alreadyPressed = false;
             }
         }
     }
